ec38ed6562bcc7fbaa8c0aaea6527c1c0f5a9855
Roberto Ivaldi <rdivaldi@gmail.com>
2014-07-24 14:44:55 -0500

    Adding Remote Control
    Adding remote control functionalities and CSS.  The animations run on page load, need to fix that.

--- js/global.js
+++ js/global.js
@@ -1,14 +1,38 @@
 // Global JS
 
 window.onload = function() {
+
+	var playBtn = document.getElementById('play');
+	var pauseBtn = document.getElementById('pause');
+	var rewindBtn = document.getElementById('rewind');
+
 	var player1 = document.getElementById("player1");
 	var player2 = document.getElementById("player2");
 	var player3 = document.getElementById("player3");
-	
-	TweenLite.to(player1, 2, {left:"75%"});
-	TweenLite.to(player1, 2, {top:"500px", delay:1.9});
-	TweenLite.to(player2, 2, {left:"25%"});
-	TweenLite.to(player2, 2, {top:"500px", delay:1.9});
-	TweenLite.to(player3, 2, {left:"50%"});
-	TweenLite.to(player3, 2, {top:"300px", delay:1.9});
+
+	var player1_movement = new TweenLite(player1, 5, {left:"75%"});
+	var player2_movement = new TweenLite(player2, 5, {left:"25%"});
+	var player3_movement = new TweenLite(player3, 5, {left:"50%"});
+
+	playBtn.onclick = function() {
+		player1_movement.play();
+		player2_movement.play();
+		player3_movement.play();
+	}
+
+	pauseBtn.onclick = function() {
+		player1_movement.pause();
+		player2_movement.pause();
+		player3_movement.pause();
+	}
+
+	rewindBtn.onclick = function() {
+		player1_movement.reverse();
+		player2_movement.reverse();
+		player3_movement.reverse();
+	}
+
+	// TweenLite.to(player1, 2, {top:"500px", delay:1.9});
+	// TweenLite.to(player2, 2, {top:"500px", delay:1.9});
+	// TweenLite.to(player3, 2, {top:"300px", delay:1.9});
 }
