778c81b80cb3aa445238fc86efae287e123639c3
Roberto Ivaldi <rdivaldi@gmail.com>
2014-08-12 14:51:23 -0500

    Setting Up Array Structure for Players
    Starting structure of For Loop, placing all players on the field in an array based off their class name "player".

--- js/global.js
+++ js/global.js
@@ -5,32 +5,35 @@ window.onload = function() {
 	var playBtn = document.getElementById('play');
 	var pauseBtn = document.getElementById('pause');
 	var rewindBtn = document.getElementById('rewind');
+	var players = document.getElementsByClassName('player');
 
-	var player1 = document.getElementById("player1");
-	var player2 = document.getElementById("player2");
-	var player3 = document.getElementById("player3");
+	for (i=0; i < players.length ;i++) {
+		var playerId = players[i].getAttribute('id');
 
-	var player1_movement = new TweenLite(player1, 5, {left:"75%"});
-	var player2_movement = new TweenLite(player2, 5, {left:"25%"});
-	var player3_movement = new TweenLite(player3, 5, {left:"50%"});
-
-	playBtn.onclick = function() {
-		player1_movement.play();
-		player2_movement.play();
-		player3_movement.play();
+		console.log(playerId);
 	}
 
-	pauseBtn.onclick = function() {
-		player1_movement.pause();
-		player2_movement.pause();
-		player3_movement.pause();
-	}
+	var player1_movement = new TweenLite(players[0], 5, {left:"75%"});
+	var player2_movement = new TweenLite(players[1], 5, {left:"25%"});
+	var player3_movement = new TweenLite(players[2], 5, {left:"50%"});
 
-	rewindBtn.onclick = function() {
-		player1_movement.reverse();
-		player2_movement.reverse();
-		player3_movement.reverse();
-	}
+	// playBtn.onclick = function() {
+	// 	player1_movement.play();
+	// 	player2_movement.play();
+	// 	player3_movement.play();
+	// }
+
+	// pauseBtn.onclick = function() {
+	// 	player1_movement.pause();
+	// 	player2_movement.pause();
+	// 	player3_movement.pause();
+	// }
+
+	// rewindBtn.onclick = function() {
+	// 	player1_movement.reverse();
+	// 	player2_movement.reverse();
+	// 	player3_movement.reverse();
+	// }
 
 	// TweenLite.to(player1, 2, {top:"500px", delay:1.9});
 	// TweenLite.to(player2, 2, {top:"500px", delay:1.9});
