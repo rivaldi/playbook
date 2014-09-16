98cc3e47375558b68d87169ac220301ab9a102b1
Roberto Ivaldi <rdivaldi@gmail.com>
2014-09-15 23:35:09 -0500

    Loading JSON
    Added a JSON script that accesses the sample JSON file when the page is loaded.

--- js/global.js
+++ js/global.js
@@ -2,6 +2,25 @@
 
 window.onload = function() {
 
+	function loadJSON(callback) {   
+
+	    var xobj = new XMLHttpRequest();
+	    xobj.overrideMimeType("application/json");
+		xobj.open('GET', '../data/sample.json', true); // Replace 'my_data' with the path to your file
+		
+		xobj.onreadystatechange = function () {
+	      if (xobj.readyState == 4 && xobj.status == "200") {
+	        callback(xobj.responseText);
+	      }
+		};
+		xobj.send(null);  
+	}
+
+	loadJSON(function(response) {
+		// Parse JSON string into object
+		var actual_JSON = JSON.parse(response);
+	});
+
 	// Remote Buttons
 	var playBtn = document.getElementById('play');
 	var pauseBtn = document.getElementById('pause');
